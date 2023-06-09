7e8be0fd631285bb18fb727687289d48d549b347
Tony <tonylehuynh66@gmail.com>
2023-06-09 21:04:09 +1000

    variables created along with win conditions array

---
+++ index.js
@@ -0,0 +1,18 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartButton = document.querySelector("#restartButton");
+const winConditions = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+]
+
+// Array of placeholders
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameRunning = false;
